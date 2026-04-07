78d9b91449f446a52754a9264bb98d197c7664b5
Nayla Syachira <naylasyahirah12mm@gmail.com>
2026-04-07 10:31:02 +0800

    Pertemuan 2: Blade Template

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <div class="h1 fw-bold">Form Create Student</div>
+
+</x-app>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <div class="h1 fw-bold">Data Student</div>
+
+</x-app>
