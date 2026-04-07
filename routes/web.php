ce72fb7dbeb67299e59b0dfb7534907b6343058b
Nayla Syachira <naylasyahirah12mm@gmail.com>
2026-04-07 18:03:35 +0800

    Pertemuan 1: init project

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/student', function () {
-    return view('student.index', ['title' => 'Student']);
-});
-
-Route::get('/student/create', function () {
-    return view('student.create', ['title' => 'Create Student']);
-});
